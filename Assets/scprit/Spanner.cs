54646d57b0a0b55b86e5b1411829b8a49650c921
saikat111 <s8856010@gmail.com>
2020-04-12 12:52:43 +0600

    My first game with umity

---
+++ Assets/scprit/Spanner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spanner : MonoBehaviour
+{
+    public GameObject knife;
+
+    private float min_X = -2.7f;
+    private float max_X = 2.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    // Update is called once per frame
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+
+        GameObject k = Instantiate(knife);
+
+        float x = Random.Range(min_X, max_X);
+
+        k.transform.position = new Vector2(x, transform.position.y);
+
+        StartCoroutine(StartSpawning());
+
+    }
+}
